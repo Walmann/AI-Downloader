7e237194d702148486d5aa5fb22678f98c8bffd8
Walmann <tovare@walmann.it>
2026-06-10 13:58:42 +0200

    .

--- deps/node_installer.py
+++ deps/node_installer.py
@@ -9,11 +9,13 @@ def install_nodes(nodes: list[node_info]):
     nodes_root = "/workspace/runpod-slim/ComfyUI/custom_nodes/"
 
     for node in nodes:
+        node_dir = nodes_root + node.dir
+        print(f"Node Dir: {node_dir}")
+        
+        
         os.chdir = nodes_root
         Repo.clone_from(url=node.url, to_path=node.dir)
 
-        node_dir = nodes_root + node.dir
-        print(node_dir)
         os.chdir(node_dir)
         
         print(f"Installing requirements for {node.dir}")
